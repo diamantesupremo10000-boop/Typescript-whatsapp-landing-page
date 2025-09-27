00d4829a732439b724362c1a576cc2aabf047d0a
Carlos Hernández <diamante.supremo.10000@gmail.com>
2025-09-27 12:49:25 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+var button = document.getElementById("ctaBtn");
+button.addEventListener("click", function () {
+    alert("¡Gracias por tu interés! Tu micro negocio pronto tendrá más clientes 🚀");
+});
